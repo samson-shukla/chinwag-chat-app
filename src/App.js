fd7cb150610de1ebffcd974ed9f2722676cf46f9
Samson Shukla <samsonshukla@gmail.com>
2022-01-30 23:51:27 +0530

    initial commit - Chinwag

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/compat/app'; //firebase SDK - firebase v9 import
+import 'firebase/compat/auth';  //firestore for database - firebase v9 import
+import 'firebase/compat/firestore'; //for user authentication - firebase v9 import
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  // API Keys
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);  //useAuthState hook to track login info of a user
+
+  function SignIn() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+
+    return (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+    );
+  }
+
+  function SignOut(){
+    return auth.currentUser && (
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    );
+  }
+
+  function ChatRoom(){
+    return "Welcome to the ChatRoom"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chinwag Chat App</h1>
       </header>
+
+      <section className="chat-room">
+        {user ? <ChatRoom/> : <SignIn />}
+      </section>
     </div>
   );
 }
