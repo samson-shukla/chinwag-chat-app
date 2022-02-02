d25baf8333fb8138c8de6220263ba78010ec3f8a
Samson Shukla <samsonshukla@gmail.com>
2022-02-02 21:19:55 +0530

    linked to firebase and styled

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app'; //firebase SDK - firebase v9 import
@@ -8,15 +9,32 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // API Keys
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "ss-reactjs.firebaseapp.com",
+  projectId: "ss-reactjs",
+  storageBucket: "ss-reactjs.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "G-FC6WXH60RS"
 });
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-
   const [user] = useAuthState(auth);  //useAuthState hook to track login info of a user
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Chinwag Chat 🔥</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
 
   function SignIn() {
     const signInWithGoogle = () => {
@@ -35,21 +53,53 @@ function App() {
     );
   }
 
-  function ChatRoom(){
-    return "Welcome to the ChatRoom"
+  function ChatRoom() {
+    const scrollDown = useRef();
+    const messagesRef = firestore.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(20);
+  
+    const [messages] = useCollectionData(query, { idField: 'id' });  
+    const [formValue, setFormValue] = useState('');  
+  
+    const sendMessage = async (e) => {
+      e.preventDefault();  
+      const { uid, photoURL } = auth.currentUser;
+  
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+      })
+  
+      setFormValue('');
+      scrollDown.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  
+    return (<>
+      <main>  
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}  
+        <span ref={scrollDown}></span>  
+      </main>  
+      <form onSubmit={sendMessage}>  
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="type here" />  
+        <button type="submit" disabled={!formValue}>✈️</button>  
+      </form>
+    </>)
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Chinwag Chat App</h1>
-      </header>
+  function ChatMessage(props) {
+    const { text, uid, photoURL } = props.message;  
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+    return (<>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt="dP" />
+        <p>{text}</p>
+      </div>
+    </>)
+  }
 
-      <section className="chat-room">
-        {user ? <ChatRoom/> : <SignIn />}
-      </section>
-    </div>
-  );
 }
 
 export default App;
